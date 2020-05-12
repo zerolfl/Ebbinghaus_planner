75d2b878dcaf9d8ee9ce157901999f54193d04e3
zerolfl <569536137@qq.com>
2020-05-13 00:53:18 +0800

    modify app files

--- Ebbinghaus_planner.m
+++ Ebbinghaus_planner.m
@@ -1,17 +1,17 @@
+%
 % Input:
-% - 开始时间，day/month/year
-% - list数量
+% - 开始时间（周几）
+% - 复习总数
 %
 % Output: 学习/复习计划表
 
-start_day = 4;
-num_list = 30;
-
-% period = [0, 1, 2, 4, 7, 15];
+start_day = 1;
+num_list = 10;
+period = [0, 1, 2, 4, 7, 15];
 % period = [0, 1, 4, 7, 15];
-period = [0, 1, 4, 10];
+% period = [0, 1, 4, 10];
 
-max_data = num_list + max(period);
+max_data = num_list + max(period); % 最后以添
 num_p = length(period);
 C = cell(ceil(max_data / 7), 7);
 for idx_l = 1:num_list
@@ -26,15 +26,20 @@ for idx_l = 1:num_list
         else
             idx_w = 1;
         end
-        disp([idx_w, idx_d]);
-        str = sprintf('%s %02d', 'Unit', idx_l);
-        C{idx_w, idx_d} = [C{idx_w, idx_d}, str];
+        unit_str = sprintf('%d', idx_l);
+        if isempty(C{idx_w, idx_d})
+            C{idx_w, idx_d} = ['Unit ', unit_str];
+        else
+            C{idx_w, idx_d} = [C{idx_w, idx_d}, '+', unit_str];
+        end
     end
 end
-T = cell2table(C);
-T.Properties.VariableNames = {'Mon.', 'Tue.', 'Wes.', 'Thu.', 'Fri.', 'Sat.', 'Sun.'};
 row_names = cell(1,idx_w);
 for idx_ww = 1:idx_w
     row_names_temp = sprintf('%s %02d', 'Week', idx_ww);
     row_names{1,idx_ww} = row_names_temp;
 end
+study_table = cell2table(C);
+study_table.Properties.VariableNames = {'Mon.', 'Tue.', 'Wes.', 'Thu.', 'Fri.', 'Sat.', 'Sun.'};
+study_table.Properties.RowNames = row_names;
+disp(study_table)
--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-# 计划通
 
-根据艾宾浩斯遗忘规律制作复习周期
 
-短期记忆节点：
+# Ebbinghaus planner
 
-1. 5分钟后
-2. 30分钟后
-3. 12小时后
+根据艾宾浩斯遗忘规律制作复习周期。
 
-长期记忆节点：
+短期记忆节点：5分钟后，30分钟后，12小时后
 
-1. 1天后
-2. 2天后
-3. 4天后
-4. 7天后
-5. 15天后
+长期记忆节点：1天后，2天后，4天后，7天后，15天后
+
+## Demo
+
+### App文件
+
+运行`Ebbinghaus_planner_app`: 
+
+> 需要MATLAB R2019a之后的版本
+
+![Demo](D:\Documents\Codes\planner\figs\Demo_app.png)
+
+### .m文件
+
+运行`Ebbinghaus_planner`: 
+
+![Demo](D:\Documents\Codes\planner\figs\Demo_m.png)
